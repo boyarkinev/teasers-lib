bbde9fe7a34fd0346d2a320ab2804f790a5b5f9f
boyarkinev <boyarkinev@gmail.com>
2024-11-28 16:40:00 +0300

    fix vite.config: change base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://boyarkinev.github.io/teaser-lib/',
+  base: 'https://boyarkinev.github.io',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
