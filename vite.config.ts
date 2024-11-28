2e22a24e50d3d77ddab3c842685948b8635f7e9e
boyarkinev <boyarkinev@gmail.com>
2024-11-28 13:47:33 +0300

    install gh-pages, update configs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Deploy-ReactJS-App-With-Vite/',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
