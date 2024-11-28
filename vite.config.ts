60ebec86f28d252bdb7f328e3f8e655b62569719
boyarkinev <boyarkinev@gmail.com>
2024-11-28 17:27:08 +0300

    fix vite.config: add base url env condition

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://boyarkinev.github.io',
+  base: process.env.NODE_ENV === 'production' ? 'https://boyarkinev.github.io/teasers-lib/' : '/',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
