a00e903f53d156a5153d3b88f0bb2474e0f8bda9
boyarkinev <boyarkinev@gmail.com>
2024-12-09 00:11:58 +0300

    add husky, update configs

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js, mjs, cjs, ts, jsx, tsx}'] },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
